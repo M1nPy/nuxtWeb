e6eca98cd416c37b4ccef2507cecb65540bd4bdf
M1nPy <sundayoblivion@gmail.com>
2021-03-09 01:44:18 +0900

    scroll

--- components/History.vue
+++ components/History.vue
@@ -7,8 +7,9 @@
             class="history-wrap__list--left d-flex flex-column"
             tag="ul"
           >
-            <li v-for="i in history" :key="i.text">
+            <li v-for="(i, index) in history" :key="i.text">
               <v-card
+                :id="'itemanime' + 2 * index"
                 class="history-wrap__list--left-items mx-auto"
                 height="200"
                 width="500"
@@ -23,8 +24,9 @@
             class="history-wrap__list--right d-flex flex-column"
             tag="ul"
           >
-            <li v-for="i in history" :key="i.text">
+            <li v-for="(i, index) in history" :key="i.text">
               <v-card
+                :id="'itemanime' + (2 * index + 1)"
                 class="history-wrap__list--right-items mx-auto"
                 height="200"
                 width="500"
@@ -37,7 +39,7 @@
     </v-container>
   </div>
 </template>
-<script lang="ts">
+<script>
 import Vue from 'vue'
 export default Vue.extend({
   name: 'History',
@@ -51,6 +53,30 @@ export default Vue.extend({
       ],
     }
   },
+  mounted() {
+    for (let i = -1; i < 8; i++) {
+      this.$scrollmagic.addScene(this.getscene(i))
+    }
+  },
+  methods: {
+    getscene(index) {
+      return this.$scrollmagic
+        .scene({
+          triggerElement: '#itemanime' + String(index < 0 ? 0 : index),
+          triggerHook: 0.3,
+          offset: 220 - 130 * (index < 0 ? 0.05 : index),
+          duration: 0,
+          reverse: false,
+        })
+        .setTween('#itemanime' + String(index + 1), {
+          css: {
+            opacity: '1',
+            transform: 'translate3d(0, 0, 0)',
+          },
+        })
+        .addIndicators({ name: 'OK' })
+    },
+  },
 })
 </script>
 <style lang="scss" scoped>
@@ -60,6 +86,9 @@ export default Vue.extend({
     padding: 0;
     &-items {
       margin-top: 200px;
+      transform: translate3d(500px, 0, 0);
+      opacity: 0;
+      transition: 1s all cubic-bezier(0.39, 0.575, 0.565, 1);
     }
   }
   &__list--left {
@@ -68,6 +97,9 @@ export default Vue.extend({
 
     &-items {
       margin-bottom: 200px;
+      transform: translate3d(-500px, 0, 0);
+      opacity: 0;
+      transition: 0.5s all cubic-bezier(0.39, 0.575, 0.565, 1);
     }
   }
 }
