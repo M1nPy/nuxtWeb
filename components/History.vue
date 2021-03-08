bdebbc05e75bbb9e1ef8cbee9c9210e34f9facf0
M1nPy <sundayoblivion@gmail.com>
2021-03-09 03:57:16 +0900

    scroll enhance

--- components/History.vue
+++ components/History.vue
@@ -2,44 +2,59 @@
   <div class="history-wrap">
     <v-container>
       <v-row>
-        <v-col cols="5"
-          ><h3 class="history-wrap__header--title">History</h3></v-col
-        >
-        <v-col cols="2" align-self="center" history-wrap__header--ctext
-          ><span>スクロールで変化</span></v-col
-        >
-        <v-col cols="5"></v-col>
+        <v-card class="flex">
+          <v-card-title
+            ><h3 class="history-wrap__header--title">History</h3>
+            <v-spacer></v-spacer>
+            <v-expand-transition>
+              <v-card v-show="expand" class="mx-auto">
+                <v-card-text class="py-1"
+                  >スクロールすると表示されます</v-card-text
+                >
+              </v-card>
+            </v-expand-transition>
+            <v-spacer></v-spacer>
+            <v-btn @click="expand = !expand">ここをクリック</v-btn>
+          </v-card-title>
+          <v-img
+            height="300"
+            src="https://cdn.pixabay.com/photo/2021/02/07/09/11/sunset-5990540_640.jpg"
+          ></v-img>
+        </v-card>
       </v-row>
-      <v-row>
+      <v-row class="pt-10">
         <v-col cols="5">
           <transition-group
             class="history-wrap__list--left d-flex flex-column"
             tag="ul"
           >
-            <li v-for="(i, index) in history" :key="i.text">
+            <li v-for="(i, index) in history1" :key="i.text">
               <v-card
                 :id="'itemanime' + 2 * index"
                 class="history-wrap__list--left-items mx-auto"
-                height="200"
-                width="500"
-                >{{ i.text }}</v-card
+              >
+                <v-responsive :aspect-ratio="16 / 9">
+                  <v-card-title>{{ i.title }}</v-card-title>
+                  <v-card-text>{{ i.text }}</v-card-text></v-responsive
+                ></v-card
               >
             </li>
           </transition-group>
         </v-col>
-        <v-divider></v-divider>
+        <v-spacer></v-spacer>
         <v-col cols="5">
           <transition-group
             class="history-wrap__list--right d-flex flex-column"
             tag="ul"
           >
-            <li v-for="(i, index) in history" :key="i.text">
+            <li v-for="(i, index) in history2" :key="i.text">
               <v-card
                 :id="'itemanime' + (2 * index + 1)"
                 class="history-wrap__list--right-items mx-auto"
-                height="200"
-                width="500"
-                >{{ i.text }}</v-card
+                ><v-responsive :aspect-ratio="16 / 9">
+                  <v-card-title>{{ i.title }}</v-card-title>
+                  <v-card-text>{{ i.text }}</v-card-text>
+                </v-responsive></v-card
               >
             </li>
           </transition-group>
@@ -54,16 +69,25 @@ export default Vue.extend({
   name: 'History',
   data() {
     return {
-      history: [
-        { text: 'あいうえお' },
-        { text: 'いうえおか' },
-        { text: 'うえおかき' },
-        { text: 'えおかきく' },
+      history2: [
+        { title: '5年前期', text: 'なんもしてない。' },
+        { title: '4年前期', text: 'なんもしてない。' },
+        { title: '3年前期', text: 'なんもしてない。' },
+        { title: '2年前期', text: 'なんもしてない。' },
+        { title: '1年前期', text: 'なんもしてない。' },
       ],
+      history1: [
+        { title: '5年後期', text: 'なんもしてない。' },
+        { title: '4年後期', text: 'なんもしてない。' },
+        { title: '3年後期', text: 'なんもしてない。' },
+        { title: '2年後期', text: 'なんもしてない。' },
+        { title: '1年後期', text: 'なんもしてない。' },
+      ],
+      expand: false,
     }
   },
   mounted() {
-    for (let i = -1; i < 8; i++) {
+    for (let i = -1; i < 9; i++) {
       this.$scrollmagic.addScene(this.getscene(i))
     }
   },
@@ -72,8 +96,8 @@ export default Vue.extend({
       return this.$scrollmagic
         .scene({
           triggerElement: '#itemanime' + String(index < 0 ? 0 : index),
-          triggerHook: 0.35,
-          offset: 220 - 130 * (index < 0 ? 0.05 : index),
+          triggerHook: 0.3,
+          offset: -200 - 150 * (index < 0 ? 0.05 : index),
           duration: 0,
           reverse: false,
         })
@@ -83,7 +107,7 @@ export default Vue.extend({
             transform: 'translate3d(0, 0, 0)',
           },
         })
-      // .addIndicators({ name: 'OK' })
+        .addIndicators({ name: 'OK' })
     },
   },
 })
@@ -105,7 +129,7 @@ export default Vue.extend({
       margin-top: 200px;
       transform: translate3d(500px, 0, 0);
       opacity: 0;
-      transition: 0.5s all cubic-bezier(0.39, 0.575, 0.565, 1);
+      transition: 1s all cubic-bezier(0.39, 0.575, 0.565, 1);
     }
   }
   &__list--left {
@@ -116,7 +140,7 @@ export default Vue.extend({
       margin-bottom: 200px;
       transform: translate3d(-500px, 0, 0);
       opacity: 0;
-      transition: 0.5s all cubic-bezier(0.39, 0.575, 0.565, 1);
+      transition: 1s all cubic-bezier(0.39, 0.575, 0.565, 1);
     }
   }
 }
