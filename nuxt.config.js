29da7717aed1175294453b491e44ab9e9d7feaed
M1nPy <sundayoblivion@gmail.com>
2021-03-10 07:45:30 +0900

    env fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,7 +70,10 @@ export default {
       },
     },
   },
-
+  env: {
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   loading: {
